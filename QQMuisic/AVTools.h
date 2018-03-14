8dad2e256822b697e5d58bb3d9f263f148f48ec9
qizdyyt <qizdyyt@163.com>
2018-03-14 21:12:38 +0800

    初始化

---
+++ QQMuisic/AVTools.h
@@ -0,0 +1,21 @@
+//
+//  AVTools.h
+//  QQMuisic
+//
+//  Created by 祁子栋 on 2018/3/14.
+//  Copyright © 2018年 祁子栋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVTools : NSObject
+///播放本地音效方法
++(void)playSoundWithSoundName:(NSString *)soundName;
+
+///播放音乐方法
++(void)playMusicWithName: (NSString*)musicName;
+///暂停播放音乐
++(void)pauseMusicWithName: (NSString *)musicName;
+///停止播放音乐
++(void)stopMusicWithName: (NSString*)musicName;
+@end
---
+++ QQMuisic/AVTools.m
@@ -0,0 +1,83 @@
+//
+//  AVTools.m
+//  QQMuisic
+//
+//  Created by 祁子栋 on 2018/3/14.
+//  Copyright © 2018年 祁子栋. All rights reserved.
+//
+
+#import "AVTools.h"
+#import <AVFoundation/AVFoundation.h>
+
+@implementation AVTools
+
+static NSMutableDictionary *_soundIDS;
+static NSMutableDictionary *_players;
+
++ (void)initialize
+{
+    if (self == [AVTools class]) {
+        _soundIDS = [NSMutableDictionary dictionary];
+        _players = [NSMutableDictionary dictionary];
+    }
+}
+
++(void)playSoundWithSoundName:(NSString *)soundName {
+    //1.创建soundID
+    SystemSoundID soundID = 0;
+    //2.从字典中取出soundID
+    soundID = [_soundIDS[soundName] unsignedIntValue];
+    //3.判断soundID是否为0
+    if (soundID == 0) {
+        //3.1如果为0，生成soundID
+        CFURLRef url = (__bridge CFURLRef)([[NSBundle mainBundle] URLForResource:soundName withExtension:nil]);
+        if (url == nil) {
+            return;
+        }
+        AudioServicesCreateSystemSoundID(url, &soundID);
+        //3.2并将SoundID保存到字典中
+        [_soundIDS setObject:@(soundID) forKey:soundName];
+    }
+    //4.播放音效
+    AudioServicesPlaySystemSound(soundID);
+}
+
++(void)playMusicWithName:(NSString *)musicName {
+    
+    AVAudioPlayer *player = nil;
+    
+    player = _players[musicName];
+    
+    if (player == nil) {
+        
+        NSURL *musicUrl = [[NSBundle mainBundle] URLForResource:musicName withExtension:nil];
+        if (musicUrl == nil) {
+            return;
+        }
+        
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:musicUrl error:nil];
+        
+        [_players setObject:player forKey:musicName];
+        
+        [player prepareToPlay];
+    }
+    
+    [player play];
+}
+
++(void)pauseMusicWithName:(NSString *)musicName {
+    AVAudioPlayer *player = _players[musicName];
+    if (player) {
+        [player pause];
+    }
+}
+
++(void)stopMusicWithName:(NSString *)musicName {
+    AVAudioPlayer *player = _players[musicName];
+    if (player) {
+        [player stop];
+        [_players removeObjectForKey:musicName];
+        player = nil;
+    }
+}
+@end
